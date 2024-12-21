a17b949c5592f9d6854a81f77c5111a2dd317d1a
EpicGika <Taher.sanad2000@gmail.com>
2024-12-21 21:50:29 +0200

    add filters

--- project/product/views.py
+++ project/product/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from .models import Product
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .filters import ProductsFilter
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_all_products(request):
+    
+    filterset = ProductsFilter(request.GET, queryset=Product.objects.all().order_by('name'))
+    paginator = PageNumberPagination()
+    paginator.page_size = 2
+    count = filterset.qs.count()
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    serializer = ProductSerializer(queryset, many=True)
+    context = {"products": serializer.data, "Number of products":count}
+    return Response(context)
+
+
+@api_view(['GET'])
+def get_by_id_product(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    serializer = ProductSerializer(product, many=False)
+
+    return Response({'product': serializer.data})
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'product.apps.ProductConfig',
+    'django_filters',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
